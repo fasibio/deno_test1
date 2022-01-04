25da0532d96463d3e408904feb5369df5989d247
Fabian Simon <fabian.simon@inovex.de>
2022-01-04 14:47:02 +0100

    wip

--- main.ts
+++ main.ts
@@ -2,8 +2,40 @@ import {serve} from './lib.ts'
 const port = 8001
 
 console.log(`http://localhost:${port}`)
-serve((req) => {
-  return new Response("Hello")
+const file = await Deno.readFile("./style.css");
+serve((request) => {
+  const { pathname } = new URL(request.url);
+
+  // This is how the server works:
+  // 1. A request comes in for a specific asset.
+  // 2. We read the asset from the file system.
+  // 3. We send the asset back to the client.
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    return new Response(file, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+
+  return new Response(
+    `<html>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Example</h1>
+      </body>
+    </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    },
+  );
+
 },{
   port,
 })
